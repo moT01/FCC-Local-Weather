638eeda181ce5fa531804336f046203c1fd08f26
Tom <20648924+moT01@users.noreply.github.com>
2018-05-29 15:02:11 -0500

    Update weather.js

--- weather.js
+++ weather.js
@@ -5,11 +5,11 @@ $(document).ready(function() {
     navigator.geolocation.getCurrentPosition(function(position) {
       lat = position.coords.latitude;
       lon = position.coords.longitude;
-      weatherurl = "http://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&APPID=d11bdf68802f711df543c8e449512397";
+      weatherurl = "https://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&APPID=d11bdf68802f711df543c8e449512397";
 
       $.getJSON(weatherurl, function(weatherobj) {  
         
-        icon = "http://openweathermap.org/img/w/" + weatherobj.weather[0].icon + ".png";
+        icon = "https://openweathermap.org/img/w/" + weatherobj.weather[0].icon + ".png";
         fahrenheit = Math.round((weatherobj.main.temp - 273.15) * 1.8 + 32);
         celsius = Math.round(weatherobj.main.temp - 273.15);
         
